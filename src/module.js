c5ba9f8dcddd0bd8c07be2fb015fa9013a222975
ArtemBilas <artembilas@gmail.com>
2020-08-21 15:14:20 +0300

    Made js model for generating html template

---
+++ src/module.js
@@ -0,0 +1,9 @@
+export const module = [
+    {type: 'title', value: 'Test Title', options: {}},
+    {type: 'text', value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur error expedita hic itaque maiores minus null', options: {}},
+    {type: 'textColumns', value: [
+        '1 text',
+        '2 text',
+        '3 text'
+    ], options: {}}
+]
